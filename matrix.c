4e3861359549bb25ed4b573d8baa95efaafa9e09
caiomoreirab <caiomoreira335@gmail.com>
2024-11-28 10:54:51 -0300

    First Commit

---
+++ matrix.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+
+typedef struct
+{
+    int linhas;
+    int colunas;
+    
+} Operacoes ;
+
+
---
+++ matrix.h
@@ -0,0 +1,21 @@
+#ifndef MATRIX_H   // Verifica se OPERACOES_H ainda não foi definido.
+#define MATRIX_H   // Define OPERACOES_H para evitar múltiplas inclusões
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // Fecha a proteção.
