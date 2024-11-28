73cb3a3423c1943ce1a0815ae7b42f60b7e01b6c
caiomoreirab <caiomoreira335@gmail.com>
2024-11-28 11:32:11 -0300

    Create

--- matrix.c
+++ matrix.c
@@ -7,7 +7,35 @@ typedef struct
 {
     int linhas;
     int colunas;
+    float* v;
     
-} Operacoes ;
+} Matrix ;
+
+
+Matrix * matCreate (int n, int m){
+
+    Matrix *mat; Matrix m; Matrix m; m.linhas; n.colunas;
+
+    if ( n > 0 && m > 0 ) {
+        mat = (Matrix*) malloc(sizeof(Matrix));
+    }
+
+
+            mat = (Matrix*) malloc(sizeof(Matrix));
+
+
+    if (mat != NULL) {
+        mat->linhas = n;
+        mat->colunas= m;
+        mat->v = (float *) malloc(sizeof(float) *n*m);
+    }
+    if ( mat->v != NULL ) {
+        return mat;
+    }
+
+    free(mat);
+    
+    return NULL;
+}
 
 
